52166e242b7f9b84d39f2355f02e7b4051a36064
Diego Araujo <72672788+Dieggo775@users.noreply.github.com>
2026-04-20 17:01:38 -0300

    second commit

--- AULA05/js/cadastroClientes.js
+++ AULA05/js/cadastroClientes.js
@@ -15,6 +15,16 @@
 
     const - Cria uma variavel/objeto de nivel de escopo local no projeto, porem o valor atribuido a essa variavel/objeto nao pode ser alterado durante a execucao do projeto
 
+    Copnversao de tipos de dados:
+    String para numero
+        Number() - Converte um valor para number
+        parseInt() - Converte um valor para inteiro
+        parseFloat() - Converte um valor para float
+    numero para string
+        String() - Converte um valor para string
+        toString() - Converte um valor para string
+    
+
 */
 
 //Escopo global
@@ -24,8 +34,23 @@ const getDados = function() {
     let nome = document.getElementById('nome')
     let email = document.getElementById('email')
 
-    console.log(nome.value)
-    console.log(email.value)
+    //Zerar o background dos campos
+    nome.style.backgroundColor = '#ffffff'
+    email.style.backgroundColor = '#ffffff'
+    
+    // Validação de campos obrigatorios
+    if(nome.value == '') {
+        alert('O campo nome é obrigatório')
+        nome.style.backgroundColor = '#f11316a4'
+    }
+
+    if(email.value == '') {
+        alert('O campo email é obrigatório')
+        email.style.backgroundColor = '#f11316a4'
+    }
+
+    // console.log('Nome: ' + nome.value)
+    // console.log('Email: ' + email.value)
 }
 
 botaoSalvar.addEventListener('click', function() {
