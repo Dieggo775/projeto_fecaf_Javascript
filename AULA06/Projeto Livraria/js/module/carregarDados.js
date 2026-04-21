38810e3e735150880af2b9d3f192c8496bcc3cb9
Diego Araujo <72672788+Dieggo775@users.noreply.github.com>
2026-04-21 11:36:06 -0300

    inicio projeto livraria

---
+++ AULA06/Projeto Livraria/js/module/carregarDados.js
@@ -0,0 +1,12 @@
+/*
+*********************************************************************
+*Objetivo: Criar a comunicacao entre a pagina HTML e os dados fornecidos de livros (arquivos de dados ou API)
+*Data: 21/04/2026
+*Autor: Diego Araujo
+*Versao: 1.0
+*********************************************************************
+*/
+
+import { livros } from "./livros.js";
+
+console.log(livros)
